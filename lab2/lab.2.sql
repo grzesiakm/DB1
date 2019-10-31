e69a648567300ccdd39952bda4366220aaee67ae
Marta Grzesiak <mrtgrzesiak@gmail.com>
2019-10-31 18:25:28 +0100

    Update lab.2.sql

--- lab2/lab.2.sql
+++ lab2/lab.2.sql
@@ -4,13 +4,14 @@ CREATE TABLE  emp (empno NUMERIC(4,0) PRIMARY KEY,
                    comm NUMERIC(7,2),
                    mgr VARCHAR(10),
                    sal NUMERIC (7,2),
-                   deptno INT);
+                   deptno INT
+);
               
-INSERT INTO emp (empno, ename, job, mgr, sal, deptno) VALUES (7369, 'Smith', 'clerk', 7902, 800, 20 );
-INSERT INTO emp (empno, ename, job, mgr, sal, comm, deptno) VALUES (7499, 'Allen', 'salesman', 7698, 1600, 300, 30 );
-INSERT INTO emp (empno, ename, job, mgr, sal, comm, deptno) VALUES (7521, 'Ward', 'salesman', 7698, 1250, 500, 30 );
-INSERT INTO emp (empno, ename, job, mgr, sal, deptno) VALUES (7566, 'Jones','manager', 7839, 2975, 20 );
-INSERT INTO emp (empno, ename, job, mgr, sal, comm, deptno) VALUES (7654, 'Martin', 'salesman', 7698, 1250, 1400, 30 );
+INSERT INTO emp (empno, ename, job, mgr, sal, deptno) VALUES (7369, 'Smith', 'clerk', 7902, 800, 20);
+INSERT INTO emp (empno, ename, job, mgr, sal, comm, deptno) VALUES (7499, 'Allen', 'salesman', 7698, 1600, 300, 30);
+INSERT INTO emp (empno, ename, job, mgr, sal, comm, deptno) VALUES (7521, 'Ward', 'salesman', 7698, 1250, 500, 30);
+INSERT INTO emp (empno, ename, job, mgr, sal, deptno) VALUES (7566, 'Jones','manager', 7839, 2975, 20);
+INSERT INTO emp (empno, ename, job, mgr, sal, comm, deptno) VALUES (7654, 'Martin', 'salesman', 7698, 1250, 1400, 30);
 
 SELECT deptno, ename, mgr FROM emp;
 
